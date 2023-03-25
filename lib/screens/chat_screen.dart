4a4f8282c169f0116c4beec2d88df76596ef5dc6
aakash-301 <aakashshivanshu5@gmail.com>
2023-03-25 16:35:24 +0530

    added coud firestore and did the setu for that

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -12,6 +13,19 @@ class ChatScreen extends StatelessWidget {
           child: Text("data"),
         ),
         itemCount: 10,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (() {
+          FirebaseFirestore.instance
+              .collection('chats/fvuc3G3EX0iwO15MlMbw/messages')
+              .snapshots()
+              .listen((data) {
+            print("++++++++++++");
+            print(data.docs[0]['text']);
+            print("++++++++++++");
+          });
+        }),
       ),
     );
   }
