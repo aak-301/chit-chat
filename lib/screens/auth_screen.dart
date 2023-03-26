c752dd8dc7988a13494811ae6fde21557f9b0f6e
aakash-301 <aakashshivanshu5@gmail.com>
2023-03-26 15:45:50 +0530

    mall refactore fix

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,4 @@
-import 'package:chitchat/widgets/auth_form.dart';
+import 'package:chitchat/widgets/auth/auth_form.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
