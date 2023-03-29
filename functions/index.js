4005494fc859b9b2257a550e254b7411bc4e7991
aakash-301 <aakashshivanshu5@gmail.com>
2023-03-29 13:39:35 +0530

    added userImage in notification

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,23 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 exports.myFunction = functions.firestore
-    .document("/chat/{msg}")
-    .onCreate((snapshot, context) => {
-      const myTitle = snapshot.data().username == null ?
+  .document("/chat/{msg}")
+  .onCreate((snapshot, context) => {
+    const myTitle = snapshot.data().username == null ?
       "UserName" : snapshot.data().username;
- 
-      const myBody = snapshot.data().text == null ?
+
+    const myBody = snapshot.data().text == null ?
       "Message" : snapshot.data().text;
- 
-      return admin.messaging().send({
-        notification: {
-          title: myTitle,
-          body: myBody,
-        },
-        topic: "chat",
-      });
+
+    const myImage = snapshot.data().userImage == null ?
+      "Image" : snapshot.data().userImage;
+
+    return admin.messaging().send({
+      notification: {
+        title: myTitle,
+        body: myBody,
+        imageUrl: myImage,
+      },
+      topic: "chat",
     });
+  });
